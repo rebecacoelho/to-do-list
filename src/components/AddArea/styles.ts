93713abbee2f380e48ecefd988fab315c08095e8
Rebeca Coelho <rebecasantoscoelho@gmail.com>
2022-10-17 16:00:25 -0300

    first commit

---
+++ src/components/AddArea/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    border: 1px solid #555;
+    border-radius: 15px;
+    margin: 20px 0;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+
+
+    .image {
+        margin-right: 5px;
+    }
+
+    input {
+        border: 0;
+        outline: 0;
+        background: transparent;
+        color: #fff;
+        flex: 1;
+        font-size: 18px;
+    }
+`
